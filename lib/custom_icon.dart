7a77938ba77054244db397e7ce81cf8dab4a5964
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-16 23:08:47 +0700

    moving all const to constants.dart file

--- lib/custom_icon.dart
+++ lib/custom_icon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-const labelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+import 'constants.dart';
 
 class CustomIcon extends StatelessWidget {
   CustomIcon({@required this.icon, this.label});
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'custom_icon.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
 enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
@@ -66,6 +63,14 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               color: activeCardColor,
+              cardChild: Column(
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: labelTextStyle,
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
